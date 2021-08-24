ebaa1398d841f65944475f85ce81f2dcf571448e
ranausamadilshad <ranausamaofficial11@gmail.com>
2021-08-24 17:09:55 +0500

    Account Info

---
+++ src/Account Info/AccountInfo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AccountInfoscreen from './AccountInfoscreen'
+
+const AccountInfo = () => {
+    return (
+        <>
+          <AccountInfoscreen/>  
+        </>
+    )
+}
+
+export default AccountInfo
--- src/Pages/MyAccountPage.js
+++ src/Pages/MyAccountPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import AccountInfo from '../Account Info/AccountInfo';
 import CustomBanner from '../CustomBanner/CustomBanner';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
+
 
 const MyAccountPage = () => {
     return (
         <>
          <Navbar/>
          <CustomBanner pname="My Account"/>
+         <AccountInfo/>
          <Footer/> 
         </>
     )
