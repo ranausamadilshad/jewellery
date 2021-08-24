21ad68ff3db6f5fb7e8671f2d619c9ed1e35bb85
ranausamadilshad <ranausamaofficial11@gmail.com>
2021-08-24 16:50:42 +0500

    Shop Added

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -2,12 +2,14 @@ import React from 'react';
 import CustomBanner from '../CustomBanner/CustomBanner';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
+import Product from '../Product/Product'
 
 const Shop = () => {
     return (
         <>
         <Navbar/>
         <CustomBanner pname="Shop"/>
+        <Product/> 
         <Footer/> 
         </>
     )
