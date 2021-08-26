e0a39c33a5a2187cb6eda535bacd9d8a75caac9f
ranausamadilshad <ranausamaofficial11@gmail.com>
2021-08-26 12:48:12 +0500

    Single product Validtaion

--- src/SingleProduct/SingleProductScreen.js
+++ src/SingleProduct/SingleProductScreen.js
@@ -1,16 +1,76 @@
 import React from 'react';
 import product1 from '../Images/singlep.jpeg';
 import './SingleProduct.css';
+import {
+    Formik,
+    Form 
+  } from 'formik'
+import * as Yup from 'yup';
+import FormikControl from './FormikControl';
+
+
+
+const dropdownColor = [
+    { key: 'Color', value: '' },
+    { key: 'red', value: 'red' },
+    { key: 'white', value: 'white' },
+    { key: 'green', value: 'green' }
+  ]
+
+  const dropdownSize = [
+    { key: 'Size', value: '' },
+    { key: 'sm', value: 'sm' },
+    { key: 'md', value: 'md' },
+    { key: 'lg', value: 'lg' }
+  ]
+
+
+
+const initialValues = {
+    color: '',
+    size: '',
+    quantity: '',
+  }
+
+  const validationSchema = Yup.object({
+    color: Yup.string().required('Required'),
+    size: Yup.string().required('Required'),
+    quantity: Yup.number().required('Required'),
+  })
+
+  const onSubmit = values => {
+    console.log('Form data', values)
+    // console.log('Saved data', JSON.parse(JSON.stringify(values)))
+  }
+
+
+
+
+
+
 const SingleProductScreen = () => {
+
+  
     return (
+
         <>
+
+<Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {formik => (
+
+
+
            <section className="single_product_detail_section">
     <div className="custom_container">
         <div className="single_product_detail">
             <div className="single_product_figure">
                <img src={product1} alt="Img"/>
             </div>
-            <div className="single_product_data">
+            <Form className="single_product_data">
                  <h3>High Designer Jewellery</h3>
                  <div className="rating_fields">
                      <span><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i></span>
@@ -26,32 +86,56 @@ const SingleProductScreen = () => {
                  <div className="size_color_filels">
                      <div className="size_color">
                          <label>Material</label>
-                         <select>
-                             <option selected disabled>color</option>
-                         </select>
+                          {/* <select>
+                              <option selected disabled>color</option> 
+                             <option selected >white</option>
+                             <option selected >orange</option>
+                             <option selected >black</option>
+                         </select>  */}
+
+         <FormikControl
+            control='select'
+            name='color'
+            options={dropdownColor}
+          />
                      </div>
                      <div className="size_color">
                         <label>size</label>
-                        <select>
+                        {/* <select>
                             <option selected disabled>size</option>
-                        </select>
+                            <option selected >sm</option>
+                            <option selected >md</option>
+                            <option selected >lg</option>
+                        </select> */}
+            <FormikControl
+            control='select'
+            name='size'
+            options={dropdownSize}
+          />
                     </div>
                  </div>
 
                  <div className="size_color_filels">
                     <div className="size_color">
                      
-                       <input type="number" />
+        <FormikControl
+            control='input'
+            type='input'
+            name='quantity'
+          />
                     </div>
                     <div className="size_color">
-                      <button>Add to Cart</button>
+                      {/* <button>Add to Cart</button> */}
+                      <button type='submit'>Add to Cart</button>
                    </div>
                 </div>
 
-            </div>
+            </Form>
         </div>
     </div>
 </section> 
+  )}
+  </Formik>
         </>
     )
 }
